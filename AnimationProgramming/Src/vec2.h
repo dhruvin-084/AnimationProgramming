b583d2e95fd1c228773b70f8ab9e9ec7b05f87ef
Dhruvin <dhruvin084@gmail.com>
2021-05-13 07:52:11 +0530

    Vector classes added

---
+++ AnimationProgramming/Src/vec2.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template<typename T>
+struct Tvec2 {
+	union {
+		struct {
+			T x;
+			T y;
+		};
+		T v[2];
+	};
+
+	inline Tvec2() : x(T(0)), y(T(0)) {}
+	inline Tvec2(T _x, T _y) : x(_x), y(_y) {}
+	inline Tvec2(T* fv) : x(fv[0]), y(fv[1]) {}
+
+};
+
+typedef Tvec2<float> vec2;
+typedef Tvec2<int> ivec2;
+typedef Tvec2<unsigned int> uivec2;
